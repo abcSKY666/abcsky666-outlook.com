9cacd6741e1f8d77dee0d6fe9817c48a60c4f915
SuperSky2021 <Super_S2021@outlook.com>
2023-04-27 19:48:55 +0800

    o

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // 定义changePage函数
+var pointPageNum = null;
 function changePage(pageNum) {
 	// 获取所有页面元素和按钮元素
 	var pages = document.getElementsByClassName("page");
@@ -18,7 +19,35 @@ function changePage(pageNum) {
 	}
 
 	// 将当前按钮变为蓝色
-	document.getElementById("button" + pageNum).classList.add("active");
+	if(pageNum == "2.1" || pageNum == "2.2") {
+		pointPageNum = 2;
+	} else {
+		pointPageNum = pageNum;
+	}
+	document.getElementById("button" + pointPageNum).classList.add("active");
+}
+
+
+function changeLibraryPage(pageNum) {
+	// 获取所有页面元素和按钮元素
+	var pages = document.getElementsByClassName("library");
+	var buttons = document.getElementsByTagName("lib_button");
+
+	// 先将所有页面元素隐藏
+	for (var i = 0; i < pages.length; i++) {
+		pages[i].style.display = "none";
+	}
+
+	// 然后将对应页面元素显示出来
+	document.getElementById("library" + pageNum).style.display = "block";
+
+	// 将所有按钮恢复原来的颜色
+	for (let i = 0; i < buttons.length; i++) {
+		buttons[i].style["background-color"] = "white"; //先去除之前的样式
+	}
+
+	// 将当前按钮变为蓝色
+	document.getElementById("lib_button" + pageNum).style["background-color"] = "skyblue";
 }
 
 // 页面加载完成后执行一些操作
